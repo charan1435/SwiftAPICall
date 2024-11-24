c326041f7550611b67332ffac0366555625078e2
charan1435 <sc16101@gmail.com>
2024-11-24 14:52:14 +0530

    first logic commit

--- NetworkingWork/ContentView.swift
+++ NetworkingWork/ContentView.swift
@@ -6,14 +6,39 @@
 //
 
 import SwiftUI
+struct PostDTO : Codable, Hashable {
+    let userId: Int
+    let postId: Int
+    let title: String
+    let body: String
+    
+    enum CodingKeys: String, CodingKey{
+        case userId
+        case postId = "id"
+        case title
+        case body
+    }
+}
 
 struct ContentView: View {
+    @State var postData : [PostDTO] = []
+    @State var isLoading : Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isLoading {
+                ProgressView()
+                    .scaleEffect(2.0)
+            } else{
+                List(postData, id: \.self ){ post in
+                    VStack(alignment: .leading){
+                        Text(post.title)
+                        Text(post.body)
+                        
+                    }
+                    
+                }
+            }
+            
         }
         .padding()
     }
